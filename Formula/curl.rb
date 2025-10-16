6fbad3b73af259146b20e08797650d2e453e9e3c
John Faust <45057319+slickag@users.noreply.github.com>
2025-10-16 10:13:18 +0000

    curl: update 8.16.0 bottle.

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -26,8 +26,8 @@ class Curl < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/slickag/knickknacks"
-    rebuild 1
-    sha256 cellar: :any, arm64_tahoe: "f52c3b94424aa90c73cd86fe6c3a1134f38aa1e60de415dc153e0be8c9e0d9c7"
+    rebuild 2
+    sha256 cellar: :any, arm64_tahoe: "782603840de95e9b741b9ebb5f6c8cad1ca51e64eaa5a91b3d5c1c3e58dcc2b7"
   end
 
   head do

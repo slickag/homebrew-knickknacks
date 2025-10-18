d38fee22350ff470475bb89d4e12254931e32369
John Faust <45057319+slickag@users.noreply.github.com>
2025-10-17 20:52:56 -0700

    Updated tests.yml
    aria2: add 1.37.0 bottle & updated tests.yml

---
+++ Formula/aria2.rb
@@ -0,0 +1,83 @@
+class Aria2 < Formula
+  desc "Download with resuming and segmented downloading"
+  homepage "https://aria2.github.io/"
+  url "https://github.com/aria2/aria2/releases/download/release-1.37.0/aria2-1.37.0.tar.xz"
+  sha256 "60a420ad7085eb616cb6e2bdf0a7206d68ff3d37fb5a956dc44242eb2f79b66b"
+  license "GPL-2.0-or-later"
+
+  bottle do
+    root_url "https://ghcr.io/v2/slickag/knickknacks"
+    sha256 cellar: :any,                 arm64_tahoe:   "c32a770568a4f6e88c62c815f45320fdf9dfda517f09b7c93a01c972c06138ec"
+    sha256 cellar: :any,                 arm64_sequoia: "4b90e662984b3d4302e834b8a85167fcb4721e0c55fcd252b5f56cd7aed3bb39"
+    sha256 cellar: :any,                 arm64_sonoma:  "45f01ce046e4459d63f605bc4cc99947b2f04676f129614817d962e4e28821b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1962f0ac9e4574206fadc0ee887c37ba55e4ed659b0e06e0570f8bee2e5806d3"
+  end
+
+  head do
+    url "https://github.com/aria2/aria2.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkgconf" => :build
+
+  depends_on "c-ares"
+  depends_on "libssh2"
+  depends_on "sqlite"
+
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "openssl@3"
+  end
+
+  def install
+    ENV.cxx11
+
+    if build.head?
+      ENV.clang if OS.mac?
+      ENV.append_to_cflags "-march=native -O3 -pipe -flto=auto"
+
+      system "autoreconf", "--force", "--install", "--verbose" if build.head?
+    end
+
+    args = %w[
+      --disable-silent-rules
+      --disable-nls
+      --enable-metalink
+      --enable-bittorrent
+      --with-libcares
+      --with-libssh2
+      --with-libxml2
+      --with-libz
+      --without-gnutls
+      --without-libgcrypt
+      --without-libgmp
+      --without-libnettle
+    ]
+    if OS.mac?
+      args << "--with-appletls"
+      args << "--without-openssl"
+    else
+      args << "--without-appletls"
+      args << "--with-openssl"
+    end
+
+    system "./configure", *args, *std_configure_args
+    system "make", "install"
+
+    bash_completion.install "doc/bash_completion/aria2c"
+  end
+
+  test do
+    system bin/"aria2c", "https://brew.sh/"
+    assert_path_exists testpath/"index.html", "Failed to create index.html!"
+  end
+end
